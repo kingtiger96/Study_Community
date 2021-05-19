deb401a13e38ad0fdeda18178c68aa15f0cd02e1
kingtiger <1445079235@qq.com>
2021-05-19 19:58:25 +0800

    1.使用redis完成帖子点赞数量和用户的点赞数量统计 2.使用redis实现了粉丝列表以及关注列表

--- src/main/java/com/lican/community/service/FollowService.java
+++ src/main/java/com/lican/community/service/FollowService.java
@@ -1,9 +1,14 @@
 package com.lican.community.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FollowService {
     void follow(int userId, int entityType, int entityId);
     void unFollow(int userId, int entityType, int entityId);
     long findFolloweeCount(int userId, int entityType);
     long findFollowerCount(int entityType, int entityId);
     boolean hasFollowed(int userId, int entityType, int entityId);
+    List<Map<String,Object>> findFollowees(int userId, int offset, int limit);
+    List<Map<String,Object>> findFollowers(int userId, int offset, int limit);
 }
--- src/main/java/com/lican/community/service/impl/FollowServiceImpl.java
+++ src/main/java/com/lican/community/service/impl/FollowServiceImpl.java
@@ -1,6 +1,9 @@
 package com.lican.community.service.impl;
 
+import com.lican.community.entity.UserEntity;
 import com.lican.community.service.FollowService;
+import com.lican.community.service.UserService;
+import com.lican.community.utils.CommunityConstant;
 import com.lican.community.utils.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -9,10 +12,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
-public class FollowServiceImpl implements FollowService {
+public class FollowServiceImpl implements FollowService, CommunityConstant {
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public void follow(int userId, int entityType, int entityId) {
@@ -63,4 +71,47 @@ public class FollowServiceImpl implements FollowService {
         String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         return redisTemplate.opsForZSet().score(followeeKey,entityId) != null;
     }
+
+    @Override
+    public List<Map<String, Object>> findFollowees(int userId, int offset, int limit) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId,ENTITY_TYPE_USER);
+        Set<Integer> targetIds = redisTemplate.opsForZSet().reverseRange(followeeKey,offset,offset+limit-1);
+        if(targetIds==null){
+            return null;
+        }
+
+        List<Map<String,Object>> list = new ArrayList<>();
+        for(Integer targetId : targetIds){
+            Map<String,Object> map = new HashMap<>();
+            UserEntity user = userService.findUserById(targetId);
+            map.put("user",user);
+            Double score = redisTemplate.opsForZSet().score(followeeKey,targetId);
+            map.put("followTime",new Date(score.longValue()));
+            list.add(map);
+        }
+        return list;
+    }
+
+    @Override
+    public List<Map<String, Object>> findFollowers(int userId, int offset, int limit) {
+        String followerKey = RedisKeyUtil.getFollowerKey(userId,ENTITY_TYPE_USER);
+        Set<Integer> targetIds = redisTemplate.opsForZSet().reverseRange(followerKey,offset,offset+limit-1);
+
+        if(targetIds==null){
+            return null;
+        }
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        for(Integer targetId : targetIds){
+            Map<String, Object> map = new HashMap<>();
+            UserEntity user = userService.findUserById(targetId);
+            map.put("user",user);
+            Double score = redisTemplate.opsForZSet().score(followerKey,targetId);
+            map.put("followTime",new Date(score.longValue()));
+            list.add(map);
+        }
+        return list;
+    }
+
+
 }
